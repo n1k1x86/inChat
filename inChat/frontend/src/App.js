f8cc91732dc5fd6ffb9c805d9e33d1814369cf29
n1k1x86 <i@n1k1x86.ru>
2023-04-20 15:17:13 +0300

    refactoring

--- inChat/frontend/src/App.js
+++ inChat/frontend/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LoginForm from './components/LoginForm/LoginForm';
+import RegisterForm from './components/RegisterForm/RegisterForm';
+import styles from './App.module.css';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.body_style}>
+      <Routes>
+        <Route path="/login" element={<LoginForm />}/>
+        <Route path="/register" element={<RegisterForm />}/>
+      </Routes>
     </div>
   );
 }
